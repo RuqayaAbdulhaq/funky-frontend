4181c0122d29e3f9a48fa00f99792900b68afbe9
RuqayaAbdulhaq <rouqayaabdulhaq@gmail.com>
2024-09-10 23:17:00 +0300

    created dynamic table component

--- resources/js/Pages/admin/Blogs.jsx
+++ resources/js/Pages/admin/Blogs.jsx
@@ -1,9 +1,20 @@
 import DataTable from "@/Components/UI/DataTable";
 
+const columns = [
+    { header: 'Name', accessor: 'name' },
+    { header: 'Age', accessor: 'age' },
+    { header: 'Email', accessor: 'email' }
+  ];
+  
+  const data = [
+    { name: 'John Doe', age: 28, email: 'john@example.com' },
+    { name: 'Jane Smith', age: 34, email: 'jane@example.com' }
+  ];
+
 export default function AdminBlogs(props){
     return (
         <div className="flex justify-center items-center">
-            <DataTable/>
+            <DataTable data={data} columns={columns}/>
         </div>
     );
 }
