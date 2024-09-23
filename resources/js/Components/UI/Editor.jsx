76815a4b7feb261b4bec54c6af67e21c3f48cdcd
RuqayaAbdulhaq <rouqayaabdulhaq@gmail.com>
2024-09-23 23:42:14 +0300

    added custom input for image and link types

--- resources/js/Components/UI/Editor.jsx
+++ resources/js/Components/UI/Editor.jsx
@@ -67,19 +67,13 @@ const TiptapEditor = () => {
             case "code":
                 editor?.commands
                     .insertContent(
-                        '<div class="mb-6"><pre><code>console.log("hello")</code></pre></div>'
+                        '<div class="mb-6"><pre><code>console.log("Hello World!")</code></pre></div>'
                     );
                 break;
             case "image":
                 editor?.commands
                     .insertContent(
                         '<div class="mb-6"><img src="https://via.placeholder.com/150" /></div>'
-                    );
-                break;
-            case "url":
-                editor?.commands
-                    .insertContent(
-                        '<div class="mb-6"><a href="https://example.com">example.com</a></div>'
                     );
                 break;
             case "h1":
@@ -206,7 +200,64 @@ const TiptapEditor = () => {
                         className="p-1 h-10 w-14 block bg-white border border-gray-200 cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700"
                     />
                 </div>
+
+                <div class="relative">
+                    <input type="text" id="input-url" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                    focus:pt-6
+                    focus:pb-2
+                    [&:not(:placeholder-shown)]:pt-6
+                    [&:not(:placeholder-shown)]:pb-2
+                    autofill:pt-6
+                    autofill:pb-2" placeholder="URL"
+                    onChange={(e)=>{
+                        if(e.target.value){
+                            editor.commands.setLink({ href: e.target.value, target: '_blank' })
+                        }else{
+                            editor.commands.unsetLink()
+                        }
+                    }}/>
+                    <label for="input-url" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
+                    peer-focus:scale-90
+                    peer-focus:translate-x-0.5
+                    peer-focus:-translate-y-1.5
+                    peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+                    peer-[:not(:placeholder-shown)]:scale-90
+                    peer-[:not(:placeholder-shown)]:translate-x-0.5
+                    peer-[:not(:placeholder-shown)]:-translate-y-1.5
+                    peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 dark:text-neutral-500">URL</label>
+                </div>
                 
+            </BubbleMenu>
+
+            <BubbleMenu
+                shouldShow={(data) => {
+                    return data.editor.isActive('image');
+                }}
+                editor={editor}
+                className="flex flex-col gap-2 p-2 bg-white border border-gray-200 rounded shadow-lg"
+
+            >
+
+                <div class="relative">
+                    <input type="text" id="input-src" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                    focus:pt-6
+                    focus:pb-2
+                    [&:not(:placeholder-shown)]:pt-6
+                    [&:not(:placeholder-shown)]:pb-2
+                    autofill:pt-6
+                    autofill:pb-2" placeholder="Source"
+                    onChange={(e)=>{editor.commands.setImage({ src: e.target.value })}}/>
+                    <label for="input-src" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
+                    peer-focus:scale-90
+                    peer-focus:translate-x-0.5
+                    peer-focus:-translate-y-1.5
+                    peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+                    peer-[:not(:placeholder-shown)]:scale-90
+                    peer-[:not(:placeholder-shown)]:translate-x-0.5
+                    peer-[:not(:placeholder-shown)]:-translate-y-1.5
+                    peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 dark:text-neutral-500">Source</label>
+                </div>
+
             </BubbleMenu>
 
             {/* Plus Button and Dropdown for Adding New Section */}
@@ -236,12 +287,6 @@ const TiptapEditor = () => {
                             onClick={() => addNewSection("image")}
                         >
                             Image
-                        </button>
-                        <button
-                            className="block w-full px-4 py-2 text-left hover:bg-gray-200"
-                            onClick={() => addNewSection("url")}
-                        >
-                            URL
                         </button>
                         <button
                             className="block w-full px-4 py-2 text-left hover:bg-gray-200"
