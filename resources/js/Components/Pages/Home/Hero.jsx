66c823e19f5b329bf1392b2cfdb4996ebdaa8ae4
RuqayaAbdulhaq <rouqayaabdulhaq@gmail.com>
2024-11-12 01:33:06 +0300

    enhanced hero section

--- resources/js/Components/Pages/Home/Hero.jsx
+++ resources/js/Components/Pages/Home/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero(props){
     return(
-        <div className="py-6 px-10 rounded-xl bg-primary flex flex-col md:flex-row items-center justify-between">
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-[48px] md:text-[72px]  font-bold text-white whitespace-pre-line">
+        <div className="text-shadow-sm py-6 px-10 rounded-xl bg-primary flex flex-col md:flex-row items-center justify-between">
+            <div className="flex flex-col justify-center">
+                <h1 className="text-[48px] md:text-8xl font-bold text-white whitespace-pre-line">
                     {"Funky \n Frontend"}
                 </h1>
                 <p className="text-[0.75rem] lg:text-[1rem] text-neutral-light">Where Frontend Gets Weird and Wonderful</p>
