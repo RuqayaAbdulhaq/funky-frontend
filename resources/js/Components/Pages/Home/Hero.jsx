316df61197c163d40205ee5976500e11e46e6077
RuqayaAbdulhaq <rouqayaabdulhaq@gmail.com>
2024-12-17 01:45:42 +0300

    changed hero section font color

--- resources/js/Components/Pages/Home/Hero.jsx
+++ resources/js/Components/Pages/Home/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero(props) {
     return (
         <div className="z-[80] py-6 px-10 flex flex-col md:flex-row items-center justify-center gap-20">
             <div className="flex flex-col justify-center">
-                <h1 className="text-5xl md:text-8xl xl:text-9xl font-bold text-neutral-dark whitespace-pre-line">
+                <h1 className="text-5xl md:text-8xl xl:text-9xl font-bold text-accent-dark whitespace-pre-line">
                     {"Funky \n Frontend"}
                 </h1>
                 <p className="#B37307 text-xs sm:text-sm lg:text-lg text-accent-dark p-2">
